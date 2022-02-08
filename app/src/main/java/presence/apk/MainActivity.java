dd507362ae295b743e204030fbc38b928ce6dc77
xfzhu2008 <zxf900703@gmail.com>
2022-02-08 17:27:42 +0900

    NavigationCtrl
    2022-2-8 Andy Hide__show_fragement

--- app/src/main/java/presence/apk/MainActivity.java
+++ app/src/main/java/presence/apk/MainActivity.java
@@ -1,8 +1,9 @@
 package presence.apk;
 
 import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,15 +23,22 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        if (navHostFragment != null) {
+            //设置ShowHideFragmentNavigator
+            navController.getNavigatorProvider().addNavigator(
+                    new ShowHideFragmentNavigator(this, navHostFragment.getChildFragmentManager(), R.id.nav_host_fragment_activity_main));
+            navController.setGraph(R.navigation.mobile_navigation); //必须使用代码显示设置
+        }
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
 
