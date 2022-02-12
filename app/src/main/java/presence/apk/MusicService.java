e7e9e8a2bd35b623675dadb0766f9c940f819467
xfzhu2008 <zxf900703@gmail.com>
2022-02-12 16:11:32 +0900

    MusicList

--- app/src/main/java/presence/apk/MusicService.java
+++ app/src/main/java/presence/apk/MusicService.java
@@ -53,5 +53,4 @@ private MediaPlayer player;
         }
     }
 
-
 }
--- app/src/main/java/presence/apk/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/presence/apk/ui/dashboard/DashboardFragment.java
@@ -1,23 +1,31 @@
 package presence.apk.ui.dashboard;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
 
 import presence.apk.R;
-import presence.apk.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+/*  private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -37,11 +45,73 @@ public class DashboardFragment extends Fragment {
         });
 
         return root;
+    }*/
+
+    ListView listView;
+    String[] items;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_dashboard,container,false);
+        listView = view.findViewById(R.id.listViewSong);
+        runtimePermission();
+
+
+        return view;
     }
 
+    public void runtimePermission(){
+        Dexter.withContext(getActivity()).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        displaySongs();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findSong (File file){
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        for(File singlefile:files){
+            if(singlefile.isDirectory() && !singlefile.isHidden()){
+                arrayList.addAll(findSong(singlefile));
+            }
+            else{
+                if(singlefile.getName().endsWith(".flac")|| singlefile.getName().endsWith(".mp3")){
+                    arrayList.add(singlefile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    public void displaySongs()
+    {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+        for(int i = 0; i<mySongs.size();i++)
+        {
+            items[i] = mySongs.get(i).getName().toString().replace(".flac","").replace(".mp3","");
+
+        }
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+        listView.setAdapter(myAdapter);
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
